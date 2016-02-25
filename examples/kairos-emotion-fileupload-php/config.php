92acf19c577d8cded339c16a84385cf3b16c6156
Steve Rucker <steverucker@gmail.com>
2016-02-24 21:46:42 -0500

    add form post example

---
+++ examples/kairos-emotion-fileupload-php/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    "app_id" => "5dc219ec",
+    "app_key" => "455b7d8b977fc6d897425d39c0edcf60",
+    "api_url" => "https://api-dev.kairos.com",
+    highchartsColors => '[
+		["#ffff00"],
+    	["#ff0000"],
+    	["#000000"],
+    	["#0000ff"]
+	]',
+	"highchartsBkgColor" => '"#666666"'
+);
+
+?>
